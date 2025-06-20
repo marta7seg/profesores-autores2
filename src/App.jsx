bd24c4cf9bb4ac4ada39f55c6103190fa35f9224
marta7seg <marta@7segundos.es>
2025-06-20 13:18:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 // src/App.jsx
-import Login from './Login';       // puedes cambiar por Register si quieres
+import Login from './Login';
 // import Register from './Register';
 
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Login />
-      {/* <Register /> */}
     </div>
   );
 }
