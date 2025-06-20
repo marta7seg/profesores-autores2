1476fc07600ed7515946c8cf5f5df6e3aa7c45b5
marta7seg <marta@7segundos.es>
2025-06-20 13:17:23 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import Login from './Login';       // puedes cambiar por Register si quieres
+// import Register from './Register';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Login />
+      {/* <Register /> */}
+    </div>
+  );
+}
