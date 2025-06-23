fd42ef7495dc4b7e17a0d8d823f1c4fdd60e0bca
marta7seg <marta@7segundos.es>
2025-06-23 10:37:50 +0200

    Create home.jsx

---
+++ src/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Bienvenido a Profesores Autores 👩‍🏫📚</h1>
+    </div>
+  );
+}
