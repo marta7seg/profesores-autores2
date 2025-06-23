f57a36faba393dafa0ef63b372db74ace103386e
marta7seg <marta@7segundos.es>
2025-06-23 10:13:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
+import Register from './Register';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
